d30ff51565ca38a8ce4c0b6edd39300d03ba728b
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2024-06-30 16:57:49 +0200

    Add main method examples

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-def main():
-    print('Hello world!')
+import hydra
+import pandas as pd
+from utils.IO import write_to_csv
+
+
+@hydra.main(config_path='./config', config_name='main.yaml')
+def main(cfg):
+    '''
+    Main method of repository.
+
+    @param cfg: Hydra configuration object
+    '''
+    # EXAMPLE: how to use config
+    data = {'messages': []}
+    for _ in range(cfg.repeat):
+        data['messages'].append(cfg.message)
+
+    # EXAMPLE: how to use utils
+    df = pd.DataFrame(data)
+    write_to_csv(df, cfg.output_filename)
+
 
 if __name__ == '__main__':
-     main()
+    '''
+    Trigger the main function
+    ''' 
+    main()
